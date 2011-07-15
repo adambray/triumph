f51c8d0af8509e4a4c90d55af18878f91dab8923
Adam Bray <adam.bray@yale.edu>
2011-07-15 05:33:55 -0400

    fixing trouble loading generators

--- lib/generators/triumph/triumph_generator.rb
+++ lib/generators/triumph/triumph_generator.rb
@@ -1,5 +1,5 @@
 class TriumphGenerator < Rails::Generators::Base
-  source_root File.expand_path('../templates', __FILE__)
+  source_root File.expand_path('../../lib/db/migrate', __FILE__)
   
   def generate
     copy_file "20110711040135_create_achievements.rb", "db/migrate/#{Time.now.utc.strftime("%Y%m%d%H%M%S")}_create_achievements.rb"
--- lib/triumph.rb
+++ lib/triumph.rb
@@ -1,3 +1,4 @@
-require File.dirname(__FILE__) + 'achievements'
-require File.dirname(__FILE__) + 'app/models/achievement'
-require File.dirname(__FILE__) + 'app/models/completed_achievement'
+require File.dirname(__FILE__) + '/achievements'
+require File.dirname(__FILE__) + '/app/models/achievement'
+require File.dirname(__FILE__) + '/app/models/completed_achievement'
+require File.dirname(__FILE__) + '/generators/triumph/triumph_generator'
