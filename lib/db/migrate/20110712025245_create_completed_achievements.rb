efd7f59315600ef665a78c46e29ba92c56f45046
Adam Bray <adam.bray@yale.edu>
2011-07-15 06:26:20 -0400

    wrong migration type

--- lib/db/migrate/20110712025245_create_completed_achievements.rb
+++ lib/db/migrate/20110712025245_create_completed_achievements.rb
@@ -1,7 +1,7 @@
 class CreateCompletedAchievements < ActiveRecord::Migration
   def self.up
     create_table :completed_achievements do |t|
-      t.int     :achievement_id
+      t.integer     :achievement_id
       t.integer :user_id      
       
       t.timestamps
--- lib/db/migrate/20110712033351_create_achievement_conditions.rb
+++ lib/db/migrate/20110712033351_create_achievement_conditions.rb
@@ -1,7 +1,7 @@
 class CreateAchievementConditions < ActiveRecord::Migration
   def self.up
     create_table :achievement_conditions do |t|
-      t.int     :achievement_id
+      t.integer     :achievement_id
       t.string  :comparison_attribute
       t.string  :comparison_operator
       t.string  :comparison_value
