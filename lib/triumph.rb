768f77c9f3b838706e94f89926d7ae148268e711
Adam Bray <adam.bray@yale.edu>
2011-07-24 15:44:20 -0400

    updating to load controller properly

--- lib/triumph.rb
+++ lib/triumph.rb
@@ -1,9 +1,5 @@
 require File.dirname(__FILE__) + '/achievements'
+require File.dirname(__FILE__) + '/app/models/achievement'
+require File.dirname(__FILE__) + '/app/models/completed_achievement'
+require File.dirname(__FILE__) + '/app/controllers/achievements_controller'
 require File.dirname(__FILE__) + '/generators/triumph/triumph_generator'
-
-%w{ models controllers }.each do |dir|
-  path = File.join(File.dirname(__FILE__), 'app', dir)
-  $LOAD_PATH << path
-  ActiveSupport::Dependencies.load_paths << path
-  ActiveSupport::Dependencies.load_once_paths.delete(path)
-end
