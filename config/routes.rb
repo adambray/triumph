34e69a2023d822775f86d95acd2ada8667877541
Adam Bray <adam.bray@yale.edu>
2011-07-25 01:20:50 -0400

    fixing named paths

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do |map|
 
-  map.resources :achievements,  :controller => 'triumph/achievements', :path_prefix => "/triumph"
+  map.resources :achievements,  :controller => 'triumph/achievements', :path_prefix => "/triumph", :name_prefix => "triumph_"
 
 end
